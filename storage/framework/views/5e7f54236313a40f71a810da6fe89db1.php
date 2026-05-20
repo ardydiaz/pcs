dba989d5c826c3f087b80b91235537609dcac240
unknown <ardydiaz>
2026-05-20 11:19:45 +0800

    del

--- storage/framework/views/5e7f54236313a40f71a810da6fe89db1.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('layoutContent'); ?>
-
-<!-- Content -->
-<?php echo $__env->yieldContent('content'); ?>
-<!--/ Content -->
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts/commonMaster' , array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\faculty-evaluation\resources\views/layouts/blankLayout.blade.php ENDPATH**/ ?>
--- storage/framework/views/d20493fd7b13aede7cf6dd7b99702916.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('title', 'Blank layout - Layouts'); ?>
-
-<?php $__env->startSection('content'); ?>
-<h4 class="p-6">Blank Page</h4>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts/blankLayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\sneat\html-laravel-free-internal\full-version\resources\views/content/layouts-example/layouts-blank.blade.php ENDPATH**/ ?>
